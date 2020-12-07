eb3eb2912bf20f354fff79bc1c5385fc61491a98
allan chen <allanchen1971@gmail.com>
2020-12-07 16:58:11 +0800

    windows easy sample

---
+++ LocalSample/WindowsEazySampleForC#/TestBot/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace TestBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_SYS = new System.Windows.Forms.TextBox();
+            this.textBox_CPU = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox_IoTHubString = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_DeviceName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_Event = new System.Windows.Forms.Label();
+            this.label_SentToIoTHub = new System.Windows.Forms.Label();
+            this.button_End = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox_SYS);
+            this.panel1.Controls.Add(this.textBox_CPU);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(45, 173);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(270, 97);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox_SYS
+            // 
+            this.textBox_SYS.Location = new System.Drawing.Point(135, 64);
+            this.textBox_SYS.Name = "textBox_SYS";
+            this.textBox_SYS.Size = new System.Drawing.Size(100, 20);
+            this.textBox_SYS.TabIndex = 3;
+            // 
+            // textBox_CPU
+            // 
+            this.textBox_CPU.Location = new System.Drawing.Point(135, 19);
+            this.textBox_CPU.Name = "textBox_CPU";
+            this.textBox_CPU.Size = new System.Drawing.Size(100, 20);
+            this.textBox_CPU.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SYS Temp :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU Temp :";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox_IoTHubString);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.textBox_User);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBox_DeviceName);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(45, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(270, 132);
+            this.panel2.TabIndex = 2;
+            // 
+            // textBox_IoTHubString
+            // 
+            this.textBox_IoTHubString.Location = new System.Drawing.Point(155, 61);
+            this.textBox_IoTHubString.Name = "textBox_IoTHubString";
+            this.textBox_IoTHubString.Size = new System.Drawing.Size(100, 20);
+            this.textBox_IoTHubString.TabIndex = 9;
+            this.textBox_IoTHubString.Text = "HostName=Bot-IOTHub.azure-devices.net;DeviceId=BotTest;SharedAccessKey=uIM96ob2Vf" +
+    "RZrgXtvCiiN8GgWtedAsI2Yz/rZeMk6n8=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "IoTHub string :";
+            // 
+            // textBox_User
+            // 
+            this.textBox_User.Location = new System.Drawing.Point(155, 98);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(100, 20);
+            this.textBox_User.TabIndex = 7;
+            this.textBox_User.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "User  :";
+            // 
+            // textBox_DeviceName
+            // 
+            this.textBox_DeviceName.Location = new System.Drawing.Point(155, 22);
+            this.textBox_DeviceName.Name = "textBox_DeviceName";
+            this.textBox_DeviceName.Size = new System.Drawing.Size(100, 20);
+            this.textBox_DeviceName.TabIndex = 5;
+            this.textBox_DeviceName.Text = "BotTest";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Device Name :";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_Event
+            // 
+            this.label_Event.AutoSize = true;
+            this.label_Event.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Event.Location = new System.Drawing.Point(61, 369);
+            this.label_Event.Name = "label_Event";
+            this.label_Event.Size = new System.Drawing.Size(134, 20);
+            this.label_Event.TabIndex = 3;
+            this.label_Event.Text = "Got Command :";
+            // 
+            // label_SentToIoTHub
+            // 
+            this.label_SentToIoTHub.AutoSize = true;
+            this.label_SentToIoTHub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_SentToIoTHub.Location = new System.Drawing.Point(61, 400);
+            this.label_SentToIoTHub.Name = "label_SentToIoTHub";
+            this.label_SentToIoTHub.Size = new System.Drawing.Size(57, 20);
+            this.label_SentToIoTHub.TabIndex = 4;
+            this.label_SentToIoTHub.Text = "Sent :";
+            // 
+            // button_End
+            // 
+            this.button_End.Location = new System.Drawing.Point(225, 276);
+            this.button_End.Name = "button_End";
+            this.button_End.Size = new System.Drawing.Size(75, 23);
+            this.button_End.TabIndex = 5;
+            this.button_End.Text = "End";
+            this.button_End.UseVisualStyleBackColor = true;
+            this.button_End.Click += new System.EventHandler(this.button_End_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "test(sent data)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_End);
+            this.Controls.Add(this.label_SentToIoTHub);
+            this.Controls.Add(this.label_Event);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_SYS;
+        private System.Windows.Forms.TextBox textBox_CPU;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_DeviceName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_Event;
+        private System.Windows.Forms.Label label_SentToIoTHub;
+        private System.Windows.Forms.TextBox textBox_IoTHubString;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_End;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LocalSample/WindowsEazySampleForC#/TestBot/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.Devices.Client;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace TestBot
+{
+    public partial class Form1 : Form
+    {
+        private int tCPU = 20;
+        private int cpu_UpLimit = 50;
+        private int tSYS = 20;        
+        private static DeviceClient deviceClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                deviceClient = DeviceClient.CreateFromConnectionString(textBox_IoTHubString.Text, Microsoft.Azure.Devices.Client.TransportType.Http1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in sample: {0}", ex.Message);
+            }
+        }
+
+        async void SendMessageToCloud(DeviceClient deviceClient, string JsonString)
+        {
+            label_SentToIoTHub.Text = JsonString;
+            Microsoft.Azure.Devices.Client.Message eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(JsonString));
+            await deviceClient.SendEventAsync(eventMessage);
+        }
+
+        async void ReceiveCommands(DeviceClient deviceClient)
+        {
+            Microsoft.Azure.Devices.Client.Message receivedMessage;
+            string messageData = "";
+            receivedMessage = await deviceClient.ReceiveAsync();
+            if (receivedMessage != null)
+            {
+                try
+                {
+                    messageData = Encoding.UTF8.GetString(receivedMessage.GetBytes());
+                    await deviceClient.CompleteAsync(receivedMessage);
+                    Debug.Write("got Message : "+ messageData + "\r\n");
+                    dynamic data = JsonConvert.DeserializeObject(messageData);
+                    string VendorString = data?.__AzureSphereVendor;
+                    string commandstring = data?.__AzureSphereDMCommand;
+                    string UserName = data?.__NexcomUserName;
+                    string Devicename = data?.__AzureSphereDeviceName;
+                    string IPC_Command = data?.__IPCCOMMAND;
+                    textBox_DeviceName.Text = Devicename;
+
+                    textBox_User.Text = UserName;
+                    label_Event.Text = "Got Command : " + commandstring;
+                    string JsonString = "";
+                    if (commandstring.Equals("IPCCOMMAND", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if(IPC_Command.Equals("XCARE", StringComparison.OrdinalIgnoreCase))
+                        {
+                            var XcareData = new
+                            {
+                                CPU_Temp = Convert.ToInt16(textBox_CPU.Text),
+                                SYS_Temp = Convert.ToInt16(textBox_SYS.Text),
+                            };
+                            JsonString = JsonConvert.SerializeObject(XcareData);
+
+                            var x = new
+                            {
+                                __AzureSphereVendor = VendorString,
+                                __NexcomUserName = UserName,
+                                __AzureSphereDeviceName = textBox_DeviceName.Text,
+                                __AzureSphereDMCommand = commandstring,
+                                __IPCCOMMAND = IPC_Command,
+                                __CommandReturnStatus = 0,
+                                __ReturnValue = JsonString,
+                                __BotReturnTime = DateTime.Now.ToString("g"),
+                            };
+                            JsonString = JsonConvert.SerializeObject(x);
+                        }
+                        else if (IPC_Command.Equals("GetCPUTemp", StringComparison.OrdinalIgnoreCase))
+                        {
+                            var x = new
+                            {
+                                __AzureSphereVendor = VendorString,
+                                __NexcomUserName = UserName,
+                                __AzureSphereDeviceName = textBox_DeviceName.Text,
+                                __AzureSphereDMCommand = commandstring,
+                                __CommandReturnStatus = 0,
+                                __IPCCOMMAND = IPC_Command,
+                                __BotReturnTime = DateTime.Now.ToString("g"),
+                                __ReturnValue = "CPU Temp: " + textBox_CPU.Text + "°C",
+                            };
+                            JsonString = JsonConvert.SerializeObject(x);
+                        }
+                        else if (IPC_Command.Equals("GetSYSTemp", StringComparison.OrdinalIgnoreCase))
+                        {
+                            var x = new
+                            {
+                                __AzureSphereVendor = VendorString,
+                                __NexcomUserName = UserName,
+                                __AzureSphereDeviceName = textBox_DeviceName.Text,
+                                __AzureSphereDMCommand = commandstring,
+                                __CommandReturnStatus = 0,
+                                __IPCCOMMAND = IPC_Command,
+                                __BotReturnTime = DateTime.Now.ToString("g"),
+                                __ReturnValue = "SYS Temp: " + textBox_CPU.Text + "°C",
+                            };
+                            JsonString = JsonConvert.SerializeObject(x);
+                        }
+                        else if (IPC_Command.Equals("XCARESUPPORT", StringComparison.OrdinalIgnoreCase))
+                        {
+                            var x = new
+                            {
+                                __AzureSphereVendor = VendorString,
+                                __NexcomUserName = UserName,
+                                __AzureSphereDeviceName = textBox_DeviceName.Text,
+                                __AzureSphereDMCommand = commandstring,
+                                __CommandReturnStatus = 0,
+                                __IPCCOMMAND = IPC_Command,
+                                __BotReturnTime = DateTime.Now.ToString("g"),
+                                __ReturnValue = "XCARESUPPORTED",
+                            };
+                            JsonString = JsonConvert.SerializeObject(x);
+                        }
+                        else
+                            label_Event.Text = "Got Command : Not Support";
+                    }
+                    else
+                    {
+                        var x = new
+                        {
+                            __AzureSphereVendor = VendorString,
+                            __NexcomUserName = UserName,
+                            __AzureSphereDeviceName = textBox_DeviceName.Text,
+                            __AzureSphereDMCommand = commandstring,
+                            __CommandReturnStatus = 0,
+                            __BotReturnTime = DateTime.Now.ToString("g"),
+                            __ReturnValue = $"{commandstring} command is success",
+                        };
+                        JsonString = JsonConvert.SerializeObject(x);
+                    }
+                    Debug.Write("Sent to Cloud Message : " + JsonString + "\r\n");
+                    SendMessageToCloud(deviceClient, JsonString);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in sample: {0}", ex.Message);
+                }
+            }
+        }
+        //*********** start of Direct Method ********
+        private void ConnectionstatuschangeHandler(ConnectionState status, ConnectionStatusChangeReason reason)
+        {
+            Debug.Write("Connection status : " +status+ "\r\n");
+            Debug.Write("Connection reason : " +reason+ "\r\n");
+        }
+        private Task<MethodResponse> API_HardwareReset(MethodRequest methodRequest, object userContext)
+        {
+            Debug.Write($"\t"+ methodRequest.Name +" was called \r\n");
+            Debug.Write($"\tPayload: " + methodRequest.DataAsJson + "\r\n");
+            return Task.FromResult(new MethodResponse(new byte[0], 200));
+        }
+        private Task<MethodResponse> API_PING(MethodRequest methodRequest, object userContext)
+        {
+            Debug.Write($"\t" + methodRequest.Name + " was called \r\n");
+            Debug.Write($"\tPayload: " + methodRequest.DataAsJson + "\r\n");
+            return Task.FromResult(new MethodResponse(new byte[0], 200));
+        }
+        private Task<MethodResponse> API_SSDRecover(MethodRequest methodRequest, object userContext)
+        {
+            Debug.Write($"\t" + methodRequest.Name + " was called \r\n");
+            Debug.Write($"\tPayload: " + methodRequest.DataAsJson + "\r\n");
+            return Task.FromResult(new MethodResponse(new byte[0], 200));
+        }
+        private Task<MethodResponse> API_XCARE(MethodRequest methodRequest, object userContext)
+        {
+            Debug.Write($"\t" + methodRequest.Name + " was called \r\n");
+            Debug.Write($"\tPayload: " + methodRequest.DataAsJson + "\r\n");
+            return Task.FromResult(new MethodResponse(new byte[0], 200));
+        }
+
+        async void ReceiveDirectMethodCommands(DeviceClient deviceClient)
+        {
+            //deviceClient.SetConnectionStatusChangesHandler(ConnectionstatuschangeHandler);
+            await deviceClient.SetMethodHandlerAsync("HardwareReset", API_HardwareReset, null).ConfigureAwait(false);
+            await deviceClient.SetMethodHandlerAsync("PING", API_PING, null).ConfigureAwait(false);
+            await deviceClient.SetMethodHandlerAsync("SSDRecover", API_SSDRecover, null).ConfigureAwait(false);
+            await deviceClient.SetMethodHandlerAsync("XCARE", API_XCARE, null).ConfigureAwait(false);            
+        }
+        //*********** End of Direct Method ********
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+        }
+
+        private void button_End_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            tCPU++;
+            tSYS = rnd.Next(20,50);
+            if (tCPU > cpu_UpLimit)
+            {
+                var x = new
+                {
+                    Name = textBox_User.Text,
+                    DeviceName = textBox_DeviceName.Text,
+                    Time = DateTime.Now.ToString("g"),
+                    CPU_Temp = Convert.ToInt16(textBox_CPU.Text),
+                    SYS_Temp = Convert.ToInt16(textBox_SYS.Text),
+                };
+                string JsonString = JsonConvert.SerializeObject(x);
+                //SendMessageToCloud(deviceClient, JsonString);
+                tCPU = 20;                                
+            }
+            textBox_CPU.Text = Convert.ToString(tCPU);
+            textBox_SYS.Text = Convert.ToString(tSYS);
+            ReceiveCommands(deviceClient);
+            //ReceiveDirectMethodCommands(deviceClient);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var x = new
+            {
+                __AzureSphereVendor = "EMBUX",                
+                __NexcomUserName = "User",
+                __AzureSphereDeviceName = "ds02",
+                __AzureSphereDMCommand = "PING",
+                __CommandReturnStatus = 0,
+                __BotReturnTime = DateTime.Now.ToString("g"),
+                //CPU_Temp = tCPU++,
+                //SYS_Temp = 18,
+            };
+            string JsonString = JsonConvert.SerializeObject(x);
+            Debug.Write("Sent to Cloud Message : " + JsonString+"\r\n");
+            SendMessageToCloud(deviceClient, JsonString);
+        }
+    }
+}
